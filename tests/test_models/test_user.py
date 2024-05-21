c56fca3d65975753fcb84615eb68b7838ae2bc93
ma09ry90 <mengistumary60@gmail.com>
2024-05-21 13:25:11 -0700

    test

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,52 +1,25 @@
 #!/usr/bin/python3
-"""Unittest module for the User Class."""
-
+"""
+Test suits for amenities
+"""
+import os
+import models
 import unittest
 from datetime import datetime
-import time
-import re
-import json
-from models.engine.file_storage import FileStorage
-import os
-from models import storage
 from models.base_model import BaseModel
 
 
 class TestUser(unittest.TestCase):
+    """
+    Tests for amenities
+    """
 
-    """Test Cases for the User class."""
-
-    def setUp(self):
-        """Sets up test methods."""
+    def test_name(self):
+        """
+        Tests for name inputs
+        """
         pass
 
-    def tearDown(self):
-        """Tears down test methods."""
-        self.resetStorage()
-        pass
 
-    def resetStorage(self):
-        """Resets FileStorage data."""
-        FileStorage._FileStorage__objects = {}
-        if os.path.isfile(FileStorage._FileStorage__file_path):
-            os.remove(FileStorage._FileStorage__file_path)
-
-    def test_8_instantiation(self):
-        """Tests instantiation of User class."""
-
-        b = User()
-        self.assertEqual(str(type(b)), "<class 'models.user.User'>")
-        self.assertIsInstance(b, User)
-        self.assertTrue(issubclass(type(b), BaseModel))
-
-    def test_8_attributes(self):
-        """Tests the attributes of User class."""
-        attributes = storage.attributes()["User"]
-        o = User()
-        for k, v in attributes.items():
-            self.assertTrue(hasattr(o, k))
-            self.assertEqual(type(getattr(o, k, None)), v)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
